0bb229748fa4fae3239bdf5fbf129e1b727d05c1
Faris Alhamdan <f.a.hamdan@outlook.sa>
2024-11-28 10:48:11 +0300

    3.1.2

--- watches.py
+++ watches.py
@@ -233,7 +233,17 @@ fig2.update_traces(marker=dict(line=dict(width=1, color='black')))
 st.plotly_chart(fig2)
 
 
-st.image("feature.png",  use_column_width=True)  
+st.markdown("<br><br>", unsafe_allow_html=True)
+
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.image("feature.png", use_column_width=True)
+
+with col2:
+    st.markdown("""
+    ،بعد دراسة البيانات وخصائص الساعات، تبين أن قيمة الساعة تختلف بناءً على العوامل التالية
+    """)
 
 
 # Create a dropdown menu for selecting the chart to display
